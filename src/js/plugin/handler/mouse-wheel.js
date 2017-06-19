36979b1e9c1ccb07be43fe435cd25d88db0ab4e1
Adam Marcinkowski <a.marcinkowski@macopedia.pl>
2017-06-19 12:20:17 +0200

    Add WheelPropagationX option

--- src/js/plugin/handler/mouse-wheel.js
+++ src/js/plugin/handler/mouse-wheel.js
@@ -30,6 +30,12 @@ function bindMouseWheelHandler(element, i) {
     return true;
   }
 
+  function shouldPreventXScrollbar(deltaY) {
+    var scrollLeft = element.scrollLeft;
+
+    return !((scrollLeft === 0 && deltaY > 0) || (scrollLeft >=  i.contentWidth - i.containerWidth));
+  }
+
   function getDeltaFromEvent(e) {
     var deltaX = e.deltaX;
     var deltaY = -1 * e.deltaY;
@@ -101,11 +107,25 @@ function bindMouseWheelHandler(element, i) {
     }
 
     shouldPrevent = false;
+
     if (!i.settings.useBothWheelAxes) {
       // deltaX will only be used for horizontal scrolling and deltaY will
       // only be used for vertical scrolling - this is the default
       updateScroll(element, 'top', element.scrollTop - (deltaY * i.settings.wheelSpeed));
       updateScroll(element, 'left', element.scrollLeft + (deltaX * i.settings.wheelSpeed));
+    } else if (i.settings.wheelPropagationX === true) {
+      if (deltaX) {
+        updateScroll(element, 'left', element.scrollLeft + (deltaX * i.settings.wheelSpeed));
+      } else {
+        updateScroll(element, 'left', element.scrollLeft - (deltaY * i.settings.wheelSpeed));
+      }
+
+      if (shouldPreventXScrollbar(deltaY)) {
+          e.stopPropagation();
+          e.preventDefault();
+      }
+
+
     } else if (i.scrollbarYActive && !i.scrollbarXActive) {
       // only vertical scrollbar is active and useBothWheelAxes option is
       // active, so let's scroll vertical bar using both mouse wheel axes
@@ -117,7 +137,7 @@ function bindMouseWheelHandler(element, i) {
       shouldPrevent = true;
     } else if (i.scrollbarXActive && !i.scrollbarYActive) {
       // useBothWheelAxes and only horizontal bar is active, so use both
-      // wheel axes for horizontal bar
+      // wheel axes for horizontal bar;
       if (deltaX) {
         updateScroll(element, 'left', element.scrollLeft + (deltaX * i.settings.wheelSpeed));
       } else {
